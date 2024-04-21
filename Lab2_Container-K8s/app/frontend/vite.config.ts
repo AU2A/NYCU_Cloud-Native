beda1714cbeb2eb4b046eed41e62bf5eee1aa7b2
Aura <aura.cs12@nycu.edu.tw>
2024-04-21 09:56:20 +0800

    20240421

--- Lab2_Container-K8s/app/frontend/vite.config.ts
+++ Lab2_Container-K8s/app/frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5273',
+        target: 'http://backend:5273',
         changeOrigin: true
       }
     }
