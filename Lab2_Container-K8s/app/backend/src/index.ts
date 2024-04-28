3e53e34aa98199aedda327cf1e62173a233f4f40
Aura <aura.cs12@nycu.edu.tw>
2024-04-28 15:56:55 +0800

    Add Lab2

--- Lab2_Container-K8s/app/backend/src/index.ts
+++ Lab2_Container-K8s/app/backend/src/index.ts
@@ -1,16 +1,24 @@
-import * as dotenv from 'dotenv'
+// import * as dotenv from 'dotenv'
 import { AppConfig } from './types/config'
+import { env } from 'process'
 import { serverOf, serverStart } from './server'
 
-dotenv.config()
+// dotenv.config()
 
 const server = serverOf()
 
+// const appConfig: AppConfig = {
+//   host: process.env.HOST || 'localhost',
+//   port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+//   mongoConnectionString: process.env.MONGO_CONNECTION_STRING || ''
+// }
+
 const appConfig: AppConfig = {
-  host: process.env.HOST || 'localhost',
-  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-  mongoConnectionString: process.env.MONGO_CONNECTION_STRING || ''
+  host: '0.0.0.0',
+  port: 8888,
+  mongoConnectionString: 'mongodb://'+env.DB_HOST+':27017/myMernApp'
 }
+
 
 serverStart(appConfig)(server)
   .then(() => {
--- Lab2_Container-K8s/app/frontend/vite.config.ts
+++ Lab2_Container-K8s/app/frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { env } from 'process'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -7,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5273',
+        target: 'http://'+env.BACKEND_HOST+':8888',
         changeOrigin: true
       }
     }
